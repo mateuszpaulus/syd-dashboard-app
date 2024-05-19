b00e0e6e91507ab0a14362461d98a534a97c650f
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-05-19 21:12:40 +0200

    Feature: Add toastr to display message and initialize in app with firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment.prod';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAnimations(),
+    provideToastr(),
+  ],
 };
