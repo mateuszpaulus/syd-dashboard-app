d05880f7712c745f736cb778062b496dbb625ae5
Mateusz Paulus <paulus.m.mateusz@gmail.com>
2024-05-19 21:13:06 +0200

    Feature: CRUD operations with firebase

---
+++ src/app/services/categories.service.ts
@@ -0,0 +1,75 @@
+import { Injectable } from '@angular/core';
+
+import {
+  Firestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  updateDoc,
+  doc,
+  deleteDoc,
+  onSnapshot,
+} from '@angular/fire/firestore';
+import { Category } from '../models/category';
+import { ToastrService } from 'ngx-toastr';
+import { from, map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoriesService {
+  constructor(private fireStore: Firestore, private toastr: ToastrService) {}
+
+  saveData(data: Category) {
+    const docRef = collection(this.fireStore, 'categories');
+
+    addDoc(docRef, data)
+      .then((docRef) => {
+        this.toastr.success('Category added successfully', 'Success');
+      })
+      .catch((error) => {
+        this.toastr.error(error.message, 'Error');
+      });
+  }
+
+  loadData(): Observable<{ id: string; data: { category: string } }[]> {
+    const docRef = collection(this.fireStore, 'categories');
+    return new Observable((observer) => {
+      onSnapshot(
+        query(docRef),
+        (snapshot) => {
+          const data = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: { category: doc.data()['category'] },
+          }));
+          observer.next(data);
+        },
+        (error) => {
+          observer.error(error);
+        }
+      );
+    });
+  }
+
+  updateData(id: string, editData: { category: string }) {
+    const docRef = doc(this.fireStore, 'categories', id);
+    updateDoc(docRef, editData)
+      .then(() => {
+        this.toastr.success('Category updated successfully', 'Success');
+      })
+      .catch((error) => {
+        this.toastr.error(error.message, 'Error');
+      });
+  }
+  deleteData(id: string) {
+    const docRef = doc(this.fireStore, 'categories', id);
+    deleteDoc(docRef)
+      .then(() => {
+        this.toastr.success('Category updated successfully', 'Success');
+      })
+      .catch((error) => {
+        this.toastr.error(error.message, 'Error');
+      });
+  }
+}
